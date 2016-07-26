7b50c11867fe18387a62242333f33ec5b72960ca
gmorini <gmorini@bluemetal.com>
2016-07-26 08:41:00 -0400

    Added azure publishing.

---
+++ HotPotato/HotPotato2/PotatoHub.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace HotPotato
+{
+    public class PotatoHub : Hub
+    {
+        PotatoManager _potatoManager;
+
+        public PotatoHub(PotatoManager potatoManager)
+        {
+            _potatoManager = potatoManager;
+        }
+
+        public void PassPotato()
+        {
+            string message = string.Empty;
+
+            if (_potatoManager.PassPotato(Context.ConnectionId, out message))
+            {
+                UpdateClients();
+            }
+            else
+            {
+                UpdateClientWithError(Context.ConnectionId, message);
+            }
+        }
+
+        public void AddClient()
+        {
+            _potatoManager.AddClient(Context.ConnectionId);
+            UpdateClient(Context.ConnectionId);
+        }
+
+        public void UpdateClient(string client)
+        {
+            Clients.Client(client).UpdatePotatoState(_potatoManager.ClientWithPotato, _potatoManager.HasPotato(client));
+        }
+
+        public void UpdateClientWithError(string client, string error)
+        {
+            Clients.Client(client).UpdateClientWithError(error);
+        }
+
+        public void UpdateClients()
+        {
+            Clients.Client(_potatoManager.ClientWithPotato).UpdatePotatoState(_potatoManager.ClientWithPotato, true);
+            Clients.Clients(_potatoManager.GetClientsWithoutPotato()).UpdatePotatoState(_potatoManager.ClientWithPotato, false);
+        }
+
+        public void ResetGame()
+        {
+            _potatoManager.ResetClients();
+        }
+    }
+}
---
+++ HotPotato/HotPotato2/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+using HotPotato;
+
+[assembly: OwinStartup(typeof(HotPotato2.Startup))]
+
+namespace HotPotato2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalHost.DependencyResolver.Register(typeof(PotatoHub), () => new PotatoHub(new PotatoManager()));
+
+            app.MapSignalR();
+        }
+    }
+}
