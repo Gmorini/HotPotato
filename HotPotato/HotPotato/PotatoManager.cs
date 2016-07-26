149f4129f2129326a5de34b0e0a180c99fa58cf1
gmorini <gmorini@bluemetal.com>
2016-07-26 08:25:10 -0400

    Hot Potato A Go Go Yo.

---
+++ HotPotato/HotPotato/PotatoManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotPotato
+{
+    public class PotatoManager
+    {
+        private static List<string> _clients = new List<string>();
+        private static string _clientWithPotato = string.Empty;
+        private static Random _random = new Random();
+
+        public string ClientWithPotato
+        {
+            get { return _clientWithPotato; }
+        }
+
+        public bool HasPotato(string client)
+        {
+            return _clientWithPotato == client;
+        }
+
+        public bool PassPotato(string client, out string message)
+        {
+            if (_clientWithPotato == client)
+            {
+                var otherClients = GetClientsWithoutPotato();
+
+                if (otherClients.Count > 0)
+                {
+                    _clientWithPotato = otherClients[_random.Next(otherClients.Count)];
+                    message = $"Potato sent to {_clientWithPotato}";
+                    return true;
+                }
+                {
+                    message =  "Can't send potato, Nobody else is playing!";
+                    return false;
+                }
+            }
+            else
+            {
+                message = $"Can't send potato, {_clientWithPotato} has it!";
+                return false;
+            }
+        }
+
+        public List<string> GetClients()
+        {
+            var result = new List<string>();
+            result.AddRange(_clients);
+
+            return result;
+        }
+
+        public void AddClient(string client)
+        {
+            _clients.Add(client);
+
+            if (_clientWithPotato == string.Empty)
+            {
+                _clientWithPotato = client;
+            }
+        }
+
+        public List<string> GetClientsWithoutPotato()
+        {
+            var result = new List<string>();
+            result.AddRange(_clients.Where(m => _clientWithPotato != m));
+
+            return result;
+        }
+
+        public void ResetClients()
+        {
+            _clients.Clear();
+            _clientWithPotato = string.Empty;
+        }
+    }
+}
---
+++ HotPotato/HotPotato/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(HotPotato.Startup))]
+
+namespace HotPotato
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalHost.DependencyResolver.Register(typeof(PotatoHub), () => new PotatoHub(new PotatoManager()));
+
+            app.MapSignalR();
+        }
+    }
+}
