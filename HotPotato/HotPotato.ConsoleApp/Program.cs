04245e3a3c4fde28875541cf2794150f92763853
gmorini <gmorini@bluemetal.com>
2016-07-27 08:01:25 -0400

    Console App built.

---
+++ HotPotato/HotPotato.ConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotPotato.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hubConnection = new HubConnection("http://localhost:17553");
+
+            var hotPotato = hubConnection.CreateHubProxy("PotatoHub");
+
+            hotPotato.On<string, bool>("UpdatePotatoState", (potatoHolder, iHavePotato) =>
+            {
+                if (iHavePotato)
+                {
+                    Console.WriteLine($"I Have The Potato! ");
+                }
+                else
+                {
+                    Console.WriteLine($"{potatoHolder} Has The Potato! ");
+                }
+            });
+
+            hotPotato.On("gameReset", () =>
+            {
+                Console.Clear();
+                Console.WriteLine("Game has been reset!");
+            });
+
+            hotPotato.On<string>("updateClientWithError", (error) =>
+            {
+                Console.WriteLine(error);
+            });
+
+            hubConnection.Start().Wait();
+
+            hotPotato.Invoke("addClient").Wait();
+
+            var key = ' ';
+
+            while (key != 'e')
+            {
+                if (key == 'p')
+                {
+                    hotPotato.Invoke("passPotato").Wait();
+                }
+                else if (key == 'r')
+                {
+                    hotPotato.Invoke("resetGame").Wait();
+                }
+
+                key = Console.ReadKey().KeyChar;
+            }
+        }
+    }
+}
